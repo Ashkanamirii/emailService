783ab290fc6b79afc66757f98984b230310fac0e
Ashkan Amiri <amiri.ashkan.d@gmail.com>
2021-09-03 14:42:06 +0200

    clean code .. delete index

--- src/main/java/se/nackademin/emailservice/EmailController.java
+++ src/main/java/se/nackademin/emailservice/EmailController.java
@@ -5,9 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import se.nackademin.emailservice.service.Emailservice;
 
 /**
@@ -18,6 +16,8 @@ import se.nackademin.emailservice.service.Emailservice;
  * Copyright: MIT
  */
 @Controller
+@RequestMapping("/mail")
+@CrossOrigin
 public class EmailController {
 
 	@Autowired
